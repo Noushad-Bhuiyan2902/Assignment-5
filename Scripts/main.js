e6a50e0af1dd84f70ae2d2f44350bcd6b7684f0b
Noushad Bhuiyan <noushad2902@gmail.com>
2025-03-02 03:36:31 +0600

    Completed the disabling task.

--- Scripts/main.js
+++ Scripts/main.js
@@ -15,4 +15,30 @@ function GotIt(){
 document.getElementById("Inc-1").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
+    document.getElementById("Inc-1").disabled=true;
+})
+document.getElementById("Inc-2").addEventListener('click',function(event){
+    event.preventDefault();
+    GotIt();
+    document.getElementById("Inc-2").disabled=true;
+})
+document.getElementById("Inc-3").addEventListener('click',function(event){
+    event.preventDefault();
+    GotIt();
+    document.getElementById("Inc-3").disabled=true;
+})
+document.getElementById("Inc-4").addEventListener('click',function(event){
+    event.preventDefault();
+    GotIt();
+    document.getElementById("Inc-4").disabled=true;
+})
+document.getElementById("Inc-5").addEventListener('click',function(event){
+    event.preventDefault();
+    GotIt();
+    document.getElementById("Inc-5").disabled=true;
+})
+document.getElementById("Inc-6").addEventListener('click',function(event){
+    event.preventDefault();
+    GotIt();
+    document.getElementById("Inc-6").disabled=true;
 })
