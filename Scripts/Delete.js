50afa7568464d6f42bd27608417bb182288c3ac8
Noushad Bhuiyan <noushad2902@gmail.com>
2025-03-02 15:10:34 +0600

    3 more to go

---
+++ Scripts/Delete.js
@@ -0,0 +1,4 @@
+document.getElementById("clrHist").addEventListener('click', function(event){
+    event.preventDefault();
+    document.getElementById("logContainer").innerHTML = "";
+})
--- Scripts/main.js
+++ Scripts/main.js
@@ -6,9 +6,30 @@ function GotIt(){
     alert("Board is updated.");
     document.getElementById("Comp").innerText=Completed+1;
     document.getElementById("Pend").innerText=Pending-1;
-    if(Pending-1==0){
+    if(Pending-1===0){
         alert("Congratulations!!! You have completed all the tasks.");
     }
+    return;
+}
+
+function WriteOut(id){
+
+    const Task = document.getElementById(id).innerText;
+    const p = document.createElement("p");
+    const T = new Date();
+
+    let currentHour = T.getHours();
+    let amPm = (currentHour>=12) ? "PM" : "AM"
+    if(currentHour>12)
+    {
+        currentHour=currentHour%12;
+    }
+
+    p.innerHTML = `
+    You have completed <span class="font-bold">${Task}</span> at ${currentHour}:${T.getMinutes()}:${T.getUTCSeconds()} ${amPm}.
+    `
+    const container = document.getElementById("logContainer");
+    container.appendChild(p);
 }
 
 
@@ -16,29 +37,35 @@ document.getElementById("Inc-1").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
     document.getElementById("Inc-1").disabled=true;
+    WriteOut("taskA");
 })
 document.getElementById("Inc-2").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
     document.getElementById("Inc-2").disabled=true;
+    WriteOut("taskB");
 })
 document.getElementById("Inc-3").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
     document.getElementById("Inc-3").disabled=true;
+    WriteOut("taskC");
 })
 document.getElementById("Inc-4").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
     document.getElementById("Inc-4").disabled=true;
+    WriteOut("taskD");
 })
 document.getElementById("Inc-5").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
     document.getElementById("Inc-5").disabled=true;
+    WriteOut("taskE");
 })
 document.getElementById("Inc-6").addEventListener('click',function(event){
     event.preventDefault();
     GotIt();
     document.getElementById("Inc-6").disabled=true;
+    WriteOut("taskF");
 })
