edc1471e2ba2762c53657bb89fc001a6baf05015
Noushad Bhuiyan <noushad2902@gmail.com>
2025-03-02 15:57:53 +0600

    Solved date issue

--- Scripts/Time.js
+++ Scripts/Time.js
@@ -1,2 +1,16 @@
+function DateTiming() {
+    const Month = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+    const Weekday = [
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+    ]
+    const T = new Date();
 
-console.log(typeof Date());
+    document.getElementById("DateTime").innerHTML = `
+    <p class="text-xl font-medium">${Weekday[T.getDay()]},<br>${Month[T.getMonth()]} ${T.getDate()} ${T.getFullYear()}</p>
+    `
+    
+}
+
+DateTiming();  
